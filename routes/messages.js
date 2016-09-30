b037a57761ffc686d8e28f90cd37b08ed4463ed3
Sean <seanpeter.aguirre@gmail.com>
2016-09-30 10:57:05 -0700

    auth used for Create Update Delete of Messages

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var jsonWebToken = require('jsonwebtoken');
 
 var Message = require('../models/message');
+var User = require('../models/user');
 
 router.get('/', function(req, res, next) {
 	// 'exec' means don't execute 'find' immediately, this helps with combined queries
@@ -20,20 +22,45 @@ router.get('/', function(req, res, next) {
 		});
 });
 
-router.post('/', function(req, res, next) {
-	var message = new Message({
-		content: req.body.content
+// Middleware to handle Creating, Updating, or Deleting messages if not logged in
+router.use('/', function(req, res, next) {
+	jsonWebToken.verify(req.query.token, 'secret', function(err, decoded) {
+		if (err) {
+			return res.status(404).json({
+				title: 'Authentication failed',
+				error: err
+			});
+		}
+		next();
 	});
-	message.save(function(err, result) {
+});
+
+router.post('/', function(req, res, next) {
+	var decoded = jsonWebToken.decode(req.query.token);
+	User.findById(decoded.user._id, function(err, doc) {
 		if (err) {
 			return res.status(404).json({
 				title: 'An error occurred',
 				error: err
 			});
 		}
-		res.status(201).json({
-			message: 'Saved message',
-			obj: result
+		var message = new Message({
+			content: req.body.content,
+			user: doc
+		});
+		message.save(function(err, result) {
+			if (err) {
+				return res.status(404).json({
+					title: 'An error occurred',
+					error: err
+				});
+			}
+			doc.messages.push(result);
+			doc.save();
+			res.status(201).json({
+				message: 'Saved message',
+				obj: result
+			});
 		});
 	});
 });
