0ddd57f869bebe3d1b66fdcf8015e08e8106c1ac
Sean <seanpeter.aguirre@gmail.com>
2016-09-30 09:45:20 -0700

    functionality added to assist user signup and signin

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var passwordHash = require('password-hash');
+var jsonWebToken = require('jsonwebtoken');
 
 var User = require('../models/user');
 
+// Handles the saving of a new user
 router.post('/', function(req, res, next) {
 	var user = new User({
 		firstName: req.body.firstName,
@@ -19,8 +21,41 @@ router.post('/', function(req, res, next) {
 			});
 		}
 		res.status(201).json({
-			message: 'Saved message',
+			message: 'Saved user',
 			obj: result
+		});
+	});
+});
+
+// Handles signin functionality of a user
+// If user does not exist or password is incorrect, this post method will handle those errors
+router.post('/signin', function(req, res, next) {
+	User.findOne({email: req.body.email}, function(err, doc) {
+		if (err) {
+			return res.status(404).json({
+				title: 'An error occurred',
+				error: err
+			});
+		}
+		// 'doc' here refers to the user in the database
+		if (!doc) {
+			return res.status(404).json({
+				title: 'No user found',
+				error: {message: 'User could not be found'}
+			});
+		}
+		// passwordHash is used here to decrypt the password in the server to verify if the password was correct
+		if (!passwordHash.verify(req.body.password, doc.password)) {
+			return res.status(404).json({
+				title: 'Could not sign you in',
+				error: {message: 'Invalid password'}
+			});
+		}
+		var token = jsonWebToken.sign({user: doc}, 'secret', {expiresIn: 7200});
+		res.status(200).json({
+			message: 'Success',
+			token: token,
+			userId: doc._id
 		});
 	});
 });
